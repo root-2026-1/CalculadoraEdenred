ad37ad29131977c697b86661d4c8f3066d68b409
Marília Liz <lizmarilia07@gmail.com>
2026-04-28 18:25:34 -0300

    setup inicial funcional com Java 17, Spring Boot e Postgres

---
+++ src/main/java/com/root/calculadoraedenred/CalculadoraedenredApplication.java
@@ -0,0 +1,13 @@
+package com.root.calculadoraedenred;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraedenredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraedenredApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/root/calculadoraedenred/CalculadoraedenredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.root.calculadoraedenred;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraedenredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
